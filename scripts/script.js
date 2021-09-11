c50e9f89d17b1fba82d3427c500bc1d36ad30027
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-11 09:49:23 +0100

    finish desktop version2.0 with refined accordion

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,14 @@
+const buttons = document.querySelectorAll('button');
 
-let buttons = document.querySelectorAll('button');
+// Let all buttons listen for the click event
 buttons.forEach(button => {
-    button.addEventListener('click', function (event) {
-        event.target.nextSibling.nextElementSibling.classList.toggle('open')
-    })
+    button.addEventListener('click', togglePanel)
 })
+
+function togglePanel(event) {
+    let panel = event.target.nextElementSibling
+    
+    // add additional 30px to max-height to accomodate for margins & paddings
+    panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + 30 + 'px'
+    event.target.classList.toggle('btn--active')
+}
