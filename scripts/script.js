cd4de771c52e48444df011849e06089dad1b5132
Tomiwa Orimoloye <gabeorimoloye@gmail.com>
2021-09-11 10:54:25 +0100

    Refine accordion animation

--- scripts/script.js
+++ scripts/script.js
@@ -11,4 +11,12 @@ function togglePanel(event) {
     // add additional 30px to max-height to accomodate for margins & paddings
     panel.style.maxHeight ? panel.style.maxHeight = null : panel.style.maxHeight = panel.scrollHeight + 30 + 'px'
     event.target.classList.toggle('btn--active')
+
+    // make sure all other panels are closed
+    buttons.forEach(one => {
+        if (one != event.target && one.nextElementSibling.style.maxHeight && panel.style.maxHeight){  
+            one.nextElementSibling.style.maxHeight = null
+            one.classList.toggle('btn--active')
+        }
+    })
 }
